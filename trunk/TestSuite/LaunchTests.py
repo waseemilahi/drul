debcd53a8aaedf2d26412418326094a34a97a554
thierrybm@hotmail.com <thierrybm@hotmail.com@919be38e-956e-11dd-a953-6f7e5726be69>
2008-11-09 00:52:14 +0000

    adding folders for test suite

---
+++ trunk/TestSuite/LaunchTests.py
@@ -0,0 +1,17 @@
+"""
+DruL team, Columbia (2008) PLT class
+
+contact: tb2332@columbia.edu
+
+name: LaunchTests.py
+language: python
+programer: Thierry Bertin-Mahieux
+
+main program of the test suite, launch all tests that it can find.
+"""
+
+
+import os
+import sys
+import glob
+import time
