dd155e242dca968825297d4542bc31af301481ff
robstewart2 <robstewart2@919be38e-956e-11dd-a953-6f7e5726be69>
2008-11-19 22:08:20 +0000

    the interpreter can evaluate int arithmetic and print it

--- trunk/Parser/drul_interpreter.ml
+++ trunk/Parser/drul_interpreter.ml
@@ -2,7 +2,14 @@ open Drul_ast
 
 module NameMap = Map.Make(String)
 
-(* type t = Void | Int | Str | Bool | Pattern | Clip *)
+type pattern = bool list
+
+type t = Void
+	   | Int of int
+	   | Str of string
+	   | Bool of bool
+	   | Pattern of pattern
+	   | Clip of pattern array
 
 (*
 let rec execPrint env = function
@@ -24,18 +31,37 @@ let run = function
 	Content(statements) -> List.fold_left exec NameMap.empty statements; ignore()
 *)
 
-let evaluate e env = match e with
-		FunCall("print", [CStr(x)]) -> print_endline x
-	|	FunCall("print", [CInt(y)]) -> print_endline (string_of_int y)
-	|	FunCall("print", [CBool(z)]) -> print_endline( if z then "TRUE" else "FALSE" )
-	
+let rec evaluate e env = match e with
+		FunCall("print", [arg]) -> let v = evaluate arg env in
+		(
+			match v with
+			Str(x) -> print_endline x; Void
+			| Int(y) -> print_endline (string_of_int y); Void
+			| Bool(z) -> print_endline( if z then "TRUE" else "FALSE" ); Void
+			| _ -> print_endline("Dunno how to print this yet."); Void
+		)
+	|	CStr(x) -> Str(x)
+	|   CBool(x) -> Bool(x)
+	|   CInt(x) -> Int(x)
+	|	ArithBinop(aExp, operator, bExp) -> let aVal = evaluate aExp env in
+											let bVal = evaluate bExp env in
+											(
+												match (aVal, operator, bVal) with
+												(Int(a), Add, Int(b)) -> Int(a + b)
+												| (Int(a), Sub, Int(b)) -> Int(a - b)
+												| (Int(a), Mult, Int(b)) -> Int(a * b)
+												| (Int(a), Div, Int(b)) -> Int(a / b)
+												| (Int(a), Mod, Int(b)) -> Int(a mod b)
+												| _ -> raise (Failure("TYPE ERROR: cannot do arithmetic on non-integers"))
+											)
+	| _ -> Void
 
 let execute s env = match s with
-	Expr(e) -> evaluate e env
+	Expr(e) -> ignore(evaluate e env)
+	| _ -> ignore()
 
 let run p env = match p with
-	Content(statements) -> List.iter (fun s -> execute s env)  statements
-
+	Content(statements) -> List.iter (fun s -> execute s env) statements
 
 let _ =
 let unscoped_env = NameMap.empty in
