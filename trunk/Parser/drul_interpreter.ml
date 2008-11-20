a0636f0ce24cae0a3ee4067d8bece0627fdaf5a4
benwarfield <benwarfield@919be38e-956e-11dd-a953-6f7e5726be69>
2008-11-20 22:03:06 +0000

    Broke function calls out into their own function, and added a trap for invalid ones.

--- trunk/Parser/drul_interpreter.ml
+++ trunk/Parser/drul_interpreter.ml
@@ -3,7 +3,7 @@ open Drul_ast
 module NameMap = Map.Make(String)
 
 exception Type_error of string
-
+exception Invalid_function of string
 type pattern = bool list
 
 type t = Void
@@ -14,23 +14,10 @@ type t = Void
 	   | Clip of pattern array
 
 let rec evaluate e env = match e with
-                FunCall("pattern",[arg]) -> let v = evaluate arg env in
-                (
-                        match v with
-                        Str(x) -> Str(x)
-                 )
-                     
-       |	FunCall("print", [arg]) -> let v = evaluate arg env in
-		(
-			match v with
-			Str(x) -> print_endline x; Void
-			| Int(y) -> print_endline (string_of_int y); Void
-			| Bool(z) -> print_endline( if z then "TRUE" else "FALSE" ); Void
-			| _ -> print_endline("Dunno how to print this yet."); Void
-		)
+		FunCall(fname, fargs) -> function_call fname fargs env
 	|	CStr(x) -> Str(x)
-	|       CBool(x) -> Bool(x)
-	|       CInt(x) -> Int(x)
+	|   CBool(x) -> Bool(x)
+	|	CInt(x) -> Int(x)
 	|	Var(name) -> let symTab = fst(env) in NameMap.find name symTab  (* TODO: Multiple scopes *)
 	|	UnaryMinus(xE) -> let xV = evaluate xE env in
 		( 
@@ -80,6 +67,23 @@ let rec evaluate e env = match e with
 		)
 		
 	| _ -> Void
+and function_call fname fargs env = match (fname, fargs) with
+		"pattern",[arg] -> let v = evaluate arg env in
+			(
+				match v with
+				Str(x) -> Str(x)
+			)
+	|	("print", [arg]) -> let v = evaluate arg env in
+			(
+				match v with
+				Str(x) -> print_endline x; Void
+				| Int(y) -> print_endline (string_of_int y); Void
+				| Bool(z) -> print_endline( if z then "TRUE" else "FALSE" ); Void
+				| _ -> print_endline("Dunno how to print this yet."); Void
+			)
+	|	(other,_)	-> 
+			let msg =  "Function name '"^ other ^"' is not a valid function." in
+				raise (Invalid_function msg)
 
 let rec execute s env = match s with
 	Expr(e) -> ignore(evaluate e env); env
