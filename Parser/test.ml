c8c336047f8f5dae5ff4b30e17f70d2243f6f17a
benwarfield <benwarfield@919be38e-956e-11dd-a953-6f7e5726be69>
2008-12-03 20:19:23 +0000

    Quashed warning in test.ml.

--- Parser/test.ml
+++ Parser/test.ml
@@ -2,7 +2,7 @@ open Drul_ast
 
 let _ = 
 let lexbuf = Lexing.from_channel stdin in 
-let program = Drul_parser.program Drul_scanner.token lexbuf in 
+let _ = Drul_parser.program Drul_scanner.token lexbuf in 
 print_endline "Parsed program (somewhat) successfully!"
 (*let listing = Printer.string_of_program program in 
 print_string listing *)
