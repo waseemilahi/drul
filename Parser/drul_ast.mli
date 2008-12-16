57e4d142b52eb60d7efaa628378f37fc22163a23
benwarfield <benwarfield@919be38e-956e-11dd-a953-6f7e5726be69>
2008-12-16 22:48:59 +0000

    Made all uses of expr into tagged_expr.  Things somehow still all work.

--- Parser/drul_ast.mli
+++ Parser/drul_ast.mli
@@ -15,16 +15,16 @@ and expr =
 	|   CStr of string
 	|   CBool of bool 
 	|   Var of string 
-	|   MemberCall of expr * string * expr list
-	|   UnaryMinus of expr
-	|   UnaryNot  of expr 
-	|   ArithBinop of expr * intOp * expr
-	|   LogicBinop of expr * boolOp * expr
-	|   Comparison of expr * compOp * expr
-	|   FunCall of string * expr list
-	|   MapCall of mapper * expr list
-	|	InstrAssign of string * expr
-	|	Output of string * expr list
+	|   MemberCall of tagged_expr * string * tagged_expr list
+	|   UnaryMinus of tagged_expr
+	|   UnaryNot  of tagged_expr 
+	|   ArithBinop of tagged_expr * intOp * tagged_expr
+	|   LogicBinop of tagged_expr * boolOp * tagged_expr
+	|   Comparison of tagged_expr * compOp * tagged_expr
+	|   FunCall of string * tagged_expr list
+	|   MapCall of mapper * tagged_expr list
+	|	InstrAssign of string * tagged_expr
+	|	Output of string * tagged_expr list
 
 and statement =
 	Expr of tagged_expr
@@ -32,7 +32,7 @@ and statement =
 	| Assign of string * tagged_expr * int
 	| MapDef of string * string list * statement list * int
 	| IfBlock of tagged_expr * statement list * statement list option
-	| InstrDef of expr list * int
+	| InstrDef of tagged_expr list * int
 	| EmptyStat
 and tagged_expr = { real_expr : expr ; lineno : int }
 
